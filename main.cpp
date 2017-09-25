e092b769cd1df07148285f61094ba668c20a9112
knivsky <konrad2012.1996@tlen.pl>
2017-09-25 15:47:31 +0200

    zrobiłem cw

---
+++ main.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main(void)
+{
+     int rok_urodzenia=1993;
+     int aktualny_rok=2017;
+
+    string imie=" Mateusz ";
+    string nazwisko="Bareja";
+int wiek = aktualny_rok - rok_urodzenia;
+cout<<"Witam, nazywam sie "<< imie <<" "<< nazwisko << "i mam"<< wiek <<"lat"<<endl;
+    return 0;
+}
